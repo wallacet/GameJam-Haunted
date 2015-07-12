6f4dfb7a15a2470051d07f819acbd759a838069e
T Wallace <wallacet@email.arizona.edu>
2015-07-11 22:15:45 -0500

    Added actual explosion to explosive barrel.

--- Assets/Scripts/Huantables/ExplosiveBarrelScript.cs
+++ Assets/Scripts/Huantables/ExplosiveBarrelScript.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class ExplosiveBarrelScript : Hauntable {
 
+	#region Public Variables
+	public GameObject explosionPrefab;
+	#endregion
+
 	#region Implementation of Abstract Methods of Hauntable
 	public override void Haunt( GameObject haunter ) {
-		Debug.Log( "Ka-boom!" );
+		Destroy(this.gameObject);
+		Instantiate(explosionPrefab, this.transform.position, this.transform.rotation);
 	}
 	#endregion
 
---
+++ Assets/Scripts/Huantables/TimedDestoryScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedDestoryScript : MonoBehaviour {
+
+	#region Public Variables
+	[Tooltip( "How long until this object dies." )]
+	public float timeToLive = 1.0f;
+	#endregion
+
+	#region Unity Callbacks
+	public void Update() {
+		this.timeToLive -= Time.deltaTime;
+
+		if ( this.timeToLive <= 0 )
+			Destroy( this.gameObject );
+	}
+	#endregion
+}
