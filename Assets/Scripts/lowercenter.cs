4845976fe78afae413a3ccdc13e7a21e69b31aea
Z Saenz <zsaenz19@gmail.com>
2015-07-12 15:05:34 -0700

    Glass

---
+++ Assets/Scripts/lowercenter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lowercenter : MonoBehaviour {
+	public float centerOfMass = -0.9f;
+
+	void Start() {
+		Rigidbody rb = GetComponent<Rigidbody>();
+
+		rb.centerOfMass = new Vector3( rb.centerOfMass.x, centerOfMass, rb.centerOfMass.z );
+	}
+}
