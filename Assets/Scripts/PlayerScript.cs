b88a9317a9723c891f7905454907428d3427f077
T Wallace <wallacet@email.arizona.edu>
2015-07-11 21:31:11 -0500

    Basic haunting.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	#region Public Variables
+	[Tooltip( "How far away can the player haunt an object?" )]
+	public float maxHauntDistance = 10.0f;
+	[Tooltip( "Which physics layers should be able to be haunted?" )]
+	public LayerMask layersToHaunt;
+	#endregion
+
+	#region Private Variables
+	private Camera cam;
+	#endregion
+
+	#region Unity Callbacks
+	public void Start() {
+		this.cam = this.GetComponentInChildren<Camera>();
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void Update() {
+		if ( Input.GetButtonDown( "Haunt" ) ) {
+			RaycastHit hit;
+			if ( Physics.Raycast( this.cam.transform.position, this.cam.transform.forward, out hit, this.maxHauntDistance, this.layersToHaunt ) ) {
+				Debug.Log( "Now haunting: " + hit.collider.gameObject.name );
+			}
+		}
 	}
+	#endregion
 }
