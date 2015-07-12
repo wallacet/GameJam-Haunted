b7e4fe422be558a29fd1a9297ad42a0e59432165
T Wallace <wallacet@email.arizona.edu>
2015-07-12 18:50:27 -0500

    Added game over state.

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScript : MonoBehaviour {
+
+	#region Public Methods
+	public void RestartGame() {
+		Time.timeScale = 1.0f;
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		Score.SetScore( 0 );
+		Application.LoadLevel( 0 );
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+	#endregion
+
+	#region Unity Callbacks
+	public void Start() {
+		this.GetComponent<UnityEngine.UI.Text>().text = Score.GetScore().ToString();
+	}
+	#endregion
+}
--- Assets/Scripts/TimeLimitScript.cs
+++ Assets/Scripts/TimeLimitScript.cs
@@ -7,26 +7,38 @@ public class TimeLimitScript : MonoBehaviour {
 	#region Public Variables
 	[Tooltip( "Time in seconds until the game ends from the start." )]
 	public float startingTime = 30.0f;
+	public GameObject gameOverPrefab;
 	#endregion
 
 	#region Private Variables
 	private float timeRemaining;
 	private Text timeText;
+	private GameObject player;
+	private bool gameOver = false;
 	#endregion
 
 	#region Unity Callbacks
 	void Start() {
 		this.timeRemaining = this.startingTime;
 		this.timeText = this.GetComponent<Text>();
+		this.player = GameObject.Find( "Player" );
 	}
 
 	void Update() {
 		this.timeRemaining -= Time.deltaTime;
 
-		if ( this.timeRemaining <= 0 ) {
+		if ( this.timeRemaining <= 0 && !gameOver ) {
 			this.timeRemaining = 0;
+			this.gameOver = true;
 
 			// End the game here
+			GameObject go = Instantiate( gameOverPrefab, Vector3.zero, Quaternion.identity ) as GameObject;
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+
+			Time.timeScale = 0;
+			player.GetComponent<PlayerScript>().enabled = false;
+			player.GetComponentInChildren<MouseOrbit>().enabled = false;
 		}
 		int minutes = (int) timeRemaining / 60;
 		int seconds = (int) timeRemaining % 60;
