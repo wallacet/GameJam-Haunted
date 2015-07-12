d6a9cc5987e36286433fbde5804e0bac9252dab0
Z Saenz <zsaenz19@gmail.com>
2015-07-12 13:25:38 -0700

    Adds streetlight, basic items to knock over.

---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructable : MonoBehaviour {
+	//The same as a barrel, without an explosion.
+	#region Public Variables
+	public GameObject gibs;
+	[Tooltip( "How much random force should be given to a gib on destroy?" )]
+	public int gibForce = 50;
+	public AudioClip[] explosionSounds;
+	[Tooltip( "How much does it take for this barrel to explode?" )]
+	public float sensitivity = 1.0f;
+	[Tooltip( "How long does it take to explode after being nudged barely over our sensitivity? When hit harder, the time before exploding will be shorter." )]
+	public float timeToExplode = 5.0f;
+	[Tooltip( "How much score is this barrel worth when it explodes?" )]
+	public int scoreValue = 100;
+	#endregion
+
+	#region Private Variables
+	private Rigidbody rb;
+	private bool exploding = false;
+	#endregion
+
+	#region Public Methods
+	public void Explode() {
+		Destroy( this.gameObject );
+		GameObject gib = (GameObject) Instantiate( this.gibs, this.transform.position, this.transform.rotation );
+		Vector3 torque = this.rb.velocity;
+		torque.x += Random.Range (-gibForce,gibForce);
+		torque.y += Random.Range (-gibForce,gibForce);
+		torque.z += Random.Range (-gibForce,gibForce);
+		gib.GetComponent<Rigidbody>().AddTorque(torque);
+		if(this.explosionSounds.Length > 0)
+		{
+			gib.GetComponent<AudioSource>().clip = this.explosionSounds[Random.Range( 0, this.explosionSounds.Length )];
+			gib.GetComponent<AudioSource>().Play();
+		}
+		Score.AddScore( this.scoreValue );
+	}
+	#endregion
+
+	#region Unity Callbacks
+	public void Start() {
+		this.rb = this.GetComponent<Rigidbody>();
+	}
+
+	public void Update() {
+		// Calculating magnetude is slow; using sqrMag instead.
+		if ( !exploding && this.rb.velocity.sqrMagnitude >= sensitivity * sensitivity ) {
+			this.exploding = true;
+			// Since we don't do this every update, magnitude is ok.
+			this.Invoke( "Explode", this.timeToExplode / this.rb.velocity.magnitude );
+		}
+	}
+	#endregion
+}
