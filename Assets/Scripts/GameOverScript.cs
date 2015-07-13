02adf6d2ed24fa341cbdc4df252c74f91424986c
T Wallace <wallacet@email.arizona.edu>
2015-07-12 23:40:51 -0500

    Added end jingle.

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class GameOverScript : MonoBehaviour {
 
+	#region Private Variables
+	private AudioSource camAudio;
+	#endregion
+
 	#region Public Methods
 	public void RestartGame() {
 		Time.timeScale = 1.0f;
@@ -20,6 +24,15 @@ public class GameOverScript : MonoBehaviour {
 	#region Unity Callbacks
 	public void Start() {
 		this.GetComponent<UnityEngine.UI.Text>().text = Score.GetScore().ToString();
+		this.camAudio = GameObject.Find( "Player" ).transform.Find( "Camera" ).GetComponent<AudioSource>();
+		camAudio.volume = 0.4f;
+	}
+
+	public void Update() {
+		if( camAudio.volume > 0.1f)
+			camAudio.volume -= Time.unscaledDeltaTime * 0.333f;
+		else
+			camAudio.volume = 0.1f;
 	}
 	#endregion
 }
